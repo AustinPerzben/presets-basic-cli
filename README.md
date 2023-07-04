712d4aed9d21e9cd7c6e17d65d6155c941ed0b18
austin <aguspdc@gmail.com>
2023-07-03 20:03:00 -0700

    Update README, fix minor bugs.

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# presets-basic-cli
+# Presets Basic CLI
 A CLI tool for quickly setting up small web dev projects with useful preset templates
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Installation
+
+To install Presets Basic globally, use npm:
+
+```sh
+npm install -g presets-basic
+```
+
+This will install the CLI tool globally, so it can be run from the command line.
+
+## Usage
+
+Here's how you can use the Presets Basic CLI:
+
+### Create a New Frontend Project
+
+```sh
+presets-basic make <project-name> [-y]
+```
+
+Creates a new frontend project with the given project name. It includes an `index.html` and `style.css` file by default.
+
+- Use the `-y` option to automatically include a `script.js` file.
+- If you don't use the `-y` option, you will be prompted to decide whether or not to include a `script.js` file.
+
+Example:
+
+```sh
+presets-basic make my-new-project -y
+```
+
+### Show Help
+
+```sh
+presets-basic --help
+```
+
+or
+
+```sh
+presets-basic -h
+```
+
+Displays help information. (Basically this usage guide)
+
+### Show Version
+
+```sh
+presets-basic --version
+```
+
+or
+
+```sh
+presets-basic -V
+```
+
+Displays the currently installed version of presets-basic.
+
+## License
+
+This project is licensed under the GNU GLPv3.0 License. See the [LICENSE](./LICENSE) file for details.
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const https = require('https');
-const path = require('path');
-const readline = require('readline');
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+const readline = require("readline");
 
 const args = process.argv.slice(2);
 
@@ -15,95 +15,121 @@ Usage:
   presets-basic --version, -V               Show version
 `;
 
-const version = require('./package.json').version;
+const version = require("./package.json").version;
 
 const commands = {
-    make: (options) => {
-        const projectName = options[0];
+  make: (options) => {
+    const projectName = options[0];
 
-        if (!projectName) {
-            console.log('Usage: presets-basic make <project-name> [-y]');
-            process.exit(1);
-        }
-
-        fs.mkdirSync(projectName);
-
-        const templatesUrl = 'https://raw.githubusercontent.com/AustinPerzben/presets-basic-cli/main/templates';
-        const files = ['index.html', 'style.css'];
-        let includeScript = false;
-
-        const fetchAndWriteFile = (file) => {
-            const url = `${templatesUrl}/${file}`;
-            https.get(url, (res) => {
-                res.setEncoding('utf8');
-                let data = '';
-                res.on('data', (chunk) => {
-                    data += chunk;
-                });
-                res.on('end', () => {
-                    if (file === 'index.html') {
-                        data = data.replace('<title></title>', `<title>${projectName}</title>`);
-                        if (includeScript) {
-                            data = data.replace('</head>', '  <script src="./script.js" defer></script>\n</head>');
-                        }
-                    }
-                    fs.writeFileSync(path.join(projectName, file), data);
-                });
-            });
-        };
-
-        if (options.includes('-y')) {
-            includeScript = true;
-            files.push('script.js');
-        } else {
-            const rl = readline.createInterface({
-                input: process.stdin,
-                output: process.stdout
-            });
-
-            rl.question('Do you want a script.js file? (y/n): ', (answer) => {
-                if (answer.toLowerCase() === 'y') {
-                    files.push('script.js');
-                }
-                rl.close();
-            });
-        }
-        // Fetch and write files
-        files.forEach(fetchAndWriteFile);
-    },
-    convert: () => {
-        console.log('Coming soon...');
-    },
-    help: () => {
-        console.log(helpMessage);
-    },
-    version: () => {
-        console.log(version);
+    if (!projectName) {
+      console.log("Usage: presets-basic make <project-name> [-y]");
+      process.exit(1);
     }
+
+    fs.mkdirSync(projectName);
+
+    const templatesUrl =
+      "https://raw.githubusercontent.com/AustinPerzben/presets-basic-cli/main/templates";
+    const files = ["index.html", "style.css"];
+    let includeScript = false;
+
+    const fetchAndWriteFile = (file) => {
+      const titleCase = (str) => {
+        let upper = true;
+        let newStr = "";
+        for (let i = 0, l = str.length; i < l; i++) {
+          if (str[i] == " ") {
+            upper = true;
+            newStr += " ";
+            continue;
+          }
+          newStr += upper ? str[i].toUpperCase() : str[i].toLowerCase();
+          upper = false;
+        }
+        return newStr;
+      };
+      const url = `${templatesUrl}/${file}`;
+      https.get(url, (res) => {
+        res.setEncoding("utf8");
+        let data = "";
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+        res.on("end", () => {
+          if (file === "index.html") {
+            data = data.replace(
+              "<title></title>",
+              `<title>${titleCase(projectName)}</title>`
+            );
+            if (includeScript) {
+              data = data.replace(
+                "</head>",
+                '  <script src="./script.js" defer></script>\n</head>'
+              );
+            }
+          }
+          fs.writeFileSync(path.join(projectName, file), data);
+        });
+      });
+    };
+
+    const fetchAndWriteFiles = () => {
+      files.forEach(fetchAndWriteFile);
+    };
+
+    if (options.includes("-y")) {
+      includeScript = true;
+      files.push("script.js");
+      fetchAndWriteFiles();
+    } else {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      rl.question("Do you want a script.js file? (y/n): ", (answer) => {
+        if (answer.toLowerCase() === "y") {
+          includeScript = true;
+          files.push("script.js");
+        }
+        fetchAndWriteFiles();
+        rl.close();
+      });
+    }
+  },
+  convert: () => {
+    console.log("Coming soon...");
+  },
+  help: () => {
+    console.log(helpMessage);
+  },
+  version: () => {
+    console.log(version);
+  },
 };
 
 const main = () => {
-    const command = args[0];
+  const command = args[0];
 
-    switch (command) {
-        case 'make':
-            commands.make(args.slice(1));
-            break;
-        case 'convert':
-            commands.convert();
-            break;
-        case '--help':
-        case '-h':
-            commands.help();
-            break;
-        case '--version':
-        case '-V':
-            commands.version();
-            break;
-        default:
-            console.log('Unknown command. Use --help for usage information.');
-            break;
-    }
+  switch (command) {
+    case "make":
+      commands.make(args.slice(1));
+      break;
+    case "convert":
+      commands.convert();
+      break;
+    case "--help":
+    case "-h":
+      commands.help();
+      break;
+    case "--version":
+    case "-V":
+      commands.version();
+      break;
+    default:
+      console.log("Unknown command. Use --help for usage information.");
+      break;
+  }
 };
 
 main();
